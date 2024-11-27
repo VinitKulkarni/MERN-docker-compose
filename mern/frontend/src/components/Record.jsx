f24c2613720c0c3525e1b699561da237101abe93
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2024-11-27 22:39:22 +0530

    Update Record.jsx

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -60,7 +60,7 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://localhost:5050/record/${params.id}`, {
+        response = await fetch(`http://10.100.37.184:5050/record/${params.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
